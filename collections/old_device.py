a938a42423f7d4999caab0a647610087afbe3089
Andres Haro <hello@aharo.dev>
2024-07-06 00:46:46 -0600

    Adding an automation pipeline to run this project

--- collections/old_device.py
+++ collections/old_device.py
@@ -8,17 +8,31 @@ from dotenv import load_dotenv
 load_dotenv()
 
 from main import client, db_id, collection_admin_users_id
+from appwrite.client import Client
 from appwrite.services.databases import Databases
 import secrets
+
+project_id = os.getenv('APPWRITE_PROJECT_ID')
+api_key = os.getenv('APPWRITE_API_KEY')
+db_id = os.getenv('APPWRITE_DB_ID')
+
+client = Client()
+client \
+    .set_endpoint('https://cloud.appwrite.io/v1') \
+    .set_project(project_id) \
+    .set_key(api_key)
 
 db = Databases(client)
 
 # Create a new collection
 
-admin_users = db.create_collection(
-    database_id = db_id,
-    collection_id = secrets.token_hex(8),
-    name = 'old-devices'
-)
-
-print(admin_users)
+try:
+    # Example: Creating a collection
+    old_devices = db.create_collection(
+        database_id=db_id,
+        collection_id=secrets.token_hex(8),
+        name='old-devices'
+    )
+    print(old_devices)
+except Exception as e:
+    print(f"Error: {e}")
