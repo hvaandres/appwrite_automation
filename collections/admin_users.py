901f2a45783f0c743d40bea9dd81370ecef14a0e
Andres Haro <hello@aharo.dev>
2024-07-06 00:08:44 -0600

    Adding an automation pipeline to run this project

--- collections/admin_users.py
+++ collections/admin_users.py
@@ -1,3 +1,8 @@
+import sys
+import os
+# Add the parent directory to the Python path
+project_root = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
+sys.path.append(project_root)
 from main import client, db_id, collection_admin_users_id
 from appwrite.services.databases import Databases
 import secrets
