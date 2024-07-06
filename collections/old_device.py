fa9a46e6b74134405e73c0c3ea1ff2f632b7f904
Andres Haro <hello@aharo.dev>
2024-07-06 00:40:42 -0600

    Adding an automation pipeline to run this project

---
+++ collections/old_device.py
@@ -0,0 +1,24 @@
+import sys
+import os
+# Add the parent directory to the Python path
+project_root = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
+sys.path.append(project_root)
+
+from dotenv import load_dotenv
+load_dotenv()
+
+from main import client, db_id, collection_admin_users_id
+from appwrite.services.databases import Databases
+import secrets
+
+db = Databases(client)
+
+# Create a new collection
+
+admin_users = db.create_collection(
+    database_id = db_id,
+    collection_id = secrets.token_hex(8),
+    name = 'old-devices'
+)
+
+print(admin_users)
